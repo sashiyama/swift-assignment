f81977419f7fbd84a23099d29587c810a60f29c3
Yoshiki Sashiyama <picolt10@gmail.com>
2022-08-17 18:21:03 -0700

    Add solution1

---
+++ assignment1/problem1.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+func f(x: Decimal, y: Decimal) -> Decimal {
+    return x * y + pow(y, 2) + abs(x - y)
+}
+
+func findA(x: Decimal, y: Decimal) -> Decimal {
+    return pow(x, 2) + pow(y, 3) - 5
+}
+
+func main() {
+    print("Please enter X number")
+    let inputX = readLine()
+
+    print("Please enter Y number")
+    let inputY = readLine()
+
+    if let stringX = inputX {
+        if let stringY = inputY {
+            let optionalX = Decimal(string: stringX)
+            let optionalY = Decimal(string: stringY)
+
+            if let x = optionalX {
+                if let y = optionalY {
+                    print("F(X, Y): \(f(x: x, y: y))")
+                    print("A: \(findA(x: x, y: y))")
+                }
+            }
+        }
+    }
+}
+
+main()
