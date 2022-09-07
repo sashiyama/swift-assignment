1422df9c8750e951747c992484e17198892f55d3
Yoshiki Sashiyama <picolt10@gmail.com>
2022-09-06 20:11:24 -0700

    Add assignment6 solution2

---
+++ assignment6/problem2.swift
@@ -0,0 +1,12 @@
+enum AppleDevice {
+    static var iPhoneXS: Int { 1000 }
+    static var iPhoneXR: Int { 1100 }
+    static var iPhone8: Int { 800 }
+    static var iPhone7: Int { 750 }
+}
+
+func main() {
+    print(AppleDevice.iPhoneXS)
+}
+
+main()
