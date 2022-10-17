3dafb636fd1b7ac64bc22522f7a0862cdc507f9f
Yoshiki Sashiyama <picolt10@gmail.com>
2022-10-17 16:05:45 -0700

    Add assignemnt9 solution3

---
+++ assignment9/problem3.swift
@@ -0,0 +1,33 @@
+protocol Container {
+    associatedtype Item
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+}
+
+struct Stack<Element> {
+    var items: [Element] = []
+
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+extension Stack: Container {
+    typealias Item = Element
+    mutating func append(_ item: Element) {
+        self.push(item)
+    }
+
+    var count: Int {
+        return items.count
+    }
+
+    subscript(i: Int) -> Element {
+        return items[i]
+    }
+}
