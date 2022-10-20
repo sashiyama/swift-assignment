f65c7de9b6e521a739a7bb5f7c26dcd4a95963a8
Yoshiki Sashiyama <picolt10@gmail.com>
2022-10-20 14:19:05 -0700

    Refactor

--- assignment10/problem3.swift
+++ assignment10/problem3.swift
@@ -1,9 +1,12 @@
 // Fibo: 0, 1, 1, 2, 3, 5, 8, 13
-// fibo(n) = fibo(n - 1) + fibo(n - 2)
 // fibo(0) = 0
 // fibo(1) = 1
 // fibo(2) = 1 + 0
 // fibo(3) = fibo(2) + fibo(1) = 2
+
+// fibo(n) = 0 (n == 0)
+// fibo(n) = 1 (n == 1)
+// fibo(n) = fibo(n - 1) + fibo(n - 2) (n > 1)
 
 class Fibonacci {
     subscript(index: Int) -> Int {
