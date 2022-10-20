db9805a083b0f0f616257b81bfee873c99cc77e8
Yoshiki Sashiyama <picolt10@gmail.com>
2022-10-20 14:17:17 -0700

    Add assignment10 solution3

---
+++ assignment10/problem3.swift
@@ -0,0 +1,37 @@
+// Fibo: 0, 1, 1, 2, 3, 5, 8, 13
+// fibo(n) = fibo(n - 1) + fibo(n - 2)
+// fibo(0) = 0
+// fibo(1) = 1
+// fibo(2) = 1 + 0
+// fibo(3) = fibo(2) + fibo(1) = 2
+
+class Fibonacci {
+    subscript(index: Int) -> Int {
+        return Exec.fibo(index)
+    }
+
+    static subscript(index: Int) -> Int {
+        return Exec.fibo(index)
+    }
+
+    class Exec {
+        static func fibo(_ n: Int) -> Int {
+            if n == 0 {
+                return 0
+            } else if n == 1 {
+                return 1
+            }
+
+            return fibo(n - 1) + fibo(n - 2)
+        }
+    }
+}
+
+func main() {
+    let fibo = Fibonacci()
+    print(fibo[5])
+    let fibo1 = Fibonacci[5]
+    print(fibo1)
+}
+
+main()
